bd669dac7e934914c18cc8537834025faef969d4
Hameed M <s201424820@gmail.com>
2025-10-18 16:47:28 +0300

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<template>
+    <TresCanvas shadows window-size>
+      <TresPerspectiveCamera :position="[5, 5, 5]" />
+    </TresCanvas>
+</template>
+
+<script setup lang="ts">
+import { TresCanvas } from '@tresjs/core'
+</script>
---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// added by me
+import { templateCompilerOptions } from '@tresjs/core';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(
+      {
+        ...templateCompilerOptions
+      }
+    ),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+})
